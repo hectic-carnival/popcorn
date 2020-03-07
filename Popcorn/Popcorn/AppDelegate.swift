ae050701f56ad255d76d02e070c0a5565c9824e2
Jerrell Ezralemuel <platyunyu@gmail.com>
2020-03-08 02:58:36 +0800

    Setup hooks and formatter (#1)
    * add formatter
    
    * add hooks
    
    * reformat code

--- Popcorn/Popcorn/AppDelegate.swift
+++ Popcorn/Popcorn/AppDelegate.swift
@@ -13,9 +13,11 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
 
-
-    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+    func application(
+        _ application: UIApplication,
+        didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
+    ) -> Bool {
+        // Override point for customization after application launch. Yea.
         return true
     }
 
@@ -36,6 +38,4 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
     }
 
-
 }
-
--- README.md
+++ README.md
@@ -1,1 +1,22 @@
-# popcorn
+# Popcorn
+An XCode tooling.
+
+## Guide
+Tooling is done via `./inferno`
+
+### Setup
+Run `./inferno setup` to bootstrap.
+
+Setup script will sets up git hooks and swift-format via submodule.
+
+### Format
+Run `./inferno format` to format `Popcorn` in-place.
+Run `./inferno sformat` to run internal swift-format.
+
+
+## Configuration
+
+### Formatter
+`Tooling/formatter.json` is the default config file for `format` command. 
+[documentation](https://github.com/apple/swift-format/blob/swift-5.1-branch/Documentation/Configuration.md)
+
