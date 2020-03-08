0cde4d1db6a5cb9892b17b81222cfdd28df82686
plty <platyunyu@gmail.com>
2020-03-08 22:50:29 +0800

    remove performance measurement on UI test

--- Popcorn/PopcornUITests/PopcornUITests.swift
+++ Popcorn/PopcornUITests/PopcornUITests.swift
@@ -17,12 +17,4 @@ class PopcornUITests: XCTestCase {
         let app = XCUIApplication()
         app.launch()
     }
-
-    func testLaunchPerformance() {
-        if #available(macOS 10.15, iOS 13.0, tvOS 13.0, *) {
-            measure(metrics: [XCTOSSignpostMetric.applicationLaunch]) {
-                XCUIApplication().launch()
-            }
-        }
-    }
 }
