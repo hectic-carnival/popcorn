95fb960658d73e3115af6d4e21da66277135b5ff
plty <platyunyu@gmail.com>
2020-03-08 21:45:09 +0800

    use nicklockwood SwiftFormat

--- Popcorn/Popcorn/GameScene.swift
+++ Popcorn/Popcorn/GameScene.swift
@@ -18,20 +18,19 @@ class GameScene: SKScene {
     private var spinnyNode: SKShapeNode?
 
     override func sceneDidLoad() {
-
-        self.lastUpdateTime = 0
+        lastUpdateTime = 0
 
         // Get label node from scene and store it for use later
-        self.label = self.childNode(withName: "//helloLabel") as? SKLabelNode
+        label = childNode(withName: "//helloLabel") as? SKLabelNode
         if let label = self.label {
             label.alpha = 0.0
             label.run(SKAction.fadeIn(withDuration: 2.0))
         }
 
         // Create shape node to use during mouse interaction
-        let w = (self.size.width + self.size.height) * 0.05
-        self.spinnyNode = SKShapeNode.init(
-            rectOf: CGSize.init(width: w, height: w),
+        let w = (size.width + size.height) * 0.05
+        spinnyNode = SKShapeNode(
+            rectOf: CGSize(width: w, height: w),
             cornerRadius: w * 0.3
         )
 
@@ -54,67 +53,67 @@ class GameScene: SKScene {
     }
 
     func touchDown(atPoint pos: CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
+        if let n = spinnyNode?.copy() as! SKShapeNode? {
             n.position = pos
             n.strokeColor = SKColor.green
-            self.addChild(n)
+            addChild(n)
         }
     }
 
     func touchMoved(toPoint pos: CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
+        if let n = spinnyNode?.copy() as! SKShapeNode? {
             n.position = pos
             n.strokeColor = SKColor.blue
-            self.addChild(n)
+            addChild(n)
         }
     }
 
     func touchUp(atPoint pos: CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
+        if let n = spinnyNode?.copy() as! SKShapeNode? {
             n.position = pos
             n.strokeColor = SKColor.red
-            self.addChild(n)
+            addChild(n)
         }
     }
 
-    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+    override func touchesBegan(_ touches: Set<UITouch>, with _: UIEvent?) {
         if let label = self.label {
-            label.run(SKAction.init(named: "Pulse")!, withKey: "fadeInOut")
+            label.run(SKAction(named: "Pulse")!, withKey: "fadeInOut")
         }
 
-        for t in touches { self.touchDown(atPoint: t.location(in: self)) }
+        for t in touches { touchDown(atPoint: t.location(in: self)) }
     }
 
-    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchMoved(toPoint: t.location(in: self)) }
+    override func touchesMoved(_ touches: Set<UITouch>, with _: UIEvent?) {
+        for t in touches { touchMoved(toPoint: t.location(in: self)) }
     }
 
-    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
+    override func touchesEnded(_ touches: Set<UITouch>, with _: UIEvent?) {
+        for t in touches { touchUp(atPoint: t.location(in: self)) }
     }
 
     override func touchesCancelled(
-        _ touches: Set<UITouch>, with event: UIEvent?
+        _ touches: Set<UITouch>, with _: UIEvent?
     ) {
-        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
+        for t in touches { touchUp(atPoint: t.location(in: self)) }
     }
 
     override func update(_ currentTime: TimeInterval) {
         // Called before each frame is rendered
 
         // Initialize _lastUpdateTime if it has not already been
-        if self.lastUpdateTime == 0 {
-            self.lastUpdateTime = currentTime
+        if lastUpdateTime == 0 {
+            lastUpdateTime = currentTime
         }
 
         // Calculate time since last update
-        let dt = currentTime - self.lastUpdateTime
+        let dt = currentTime - lastUpdateTime
 
         // Update entities
-        for entity in self.entities {
+        for entity in entities {
             entity.update(deltaTime: dt)
         }
 
-        self.lastUpdateTime = currentTime
+        lastUpdateTime = currentTime
     }
 }
